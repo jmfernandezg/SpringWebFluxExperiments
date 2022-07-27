4f3b2c9a4a060ac64b6c09b54a0b626e7866723c
Jose Fernandez <jmfernandezg.awe@gmail.com>
2022-07-26 19:24:00 -0500

    Initial commit

---
+++ src/main/java/com/jmfg/xp/SpringWebFluxExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.jmfg.xp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebFluxExperimentsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebFluxExperimentsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jmfg/xp/SpringWebFluxExperimentsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jmfg.xp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebFluxExperimentsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
